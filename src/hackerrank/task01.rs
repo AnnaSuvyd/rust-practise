71bcdfdca397e50d0c3d51d4826fd74e787e50e5
AnnaSuvyd <129731273+AnnaSuvyd@users.noreply.github.com>
2025-03-19 21:24:34 +0200

    Update task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,18 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+use std::io;
+
+fn main() {
+    // Объявление переменных для ввода
+    let mut num_str_1 = String::new();
+    let mut num_str_2 = String::new();
+
+    // Читаем входные данные
+    io::stdin().read_line(&mut num_str_1).expect("Ошибка чтения");
+    io::stdin().read_line(&mut num_str_2).expect("Ошибка чтения");
+
+    // Преобразуем строки в числа
+    let num_1: i32 = num_str_1.trim().parse().expect("Ошибка преобразования");
+    let num_2: i32 = num_str_2.trim().parse().expect("Ошибка преобразования");
+
+    // Вывод суммы чисел
+    println!("{}", num_1 + num_2);
 }
