7315f59e759b93137ff45a703d67b10f001dd9e3
AnnaSuvyd <129731273+AnnaSuvyd@users.noreply.github.com>
2025-03-19 21:27:03 +0200

    Update task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,18 +1,11 @@
 use std::io;
 
 fn main() {
-    // Объявление переменных для ввода
     let mut num_str_1 = String::new();
     let mut num_str_2 = String::new();
-
-    // Читаем входные данные
-    io::stdin().read_line(&mut num_str_1).expect("Ошибка чтения");
-    io::stdin().read_line(&mut num_str_2).expect("Ошибка чтения");
-
-    // Преобразуем строки в числа
-    let num_1: i32 = num_str_1.trim().parse().expect("Ошибка преобразования");
-    let num_2: i32 = num_str_2.trim().parse().expect("Ошибка преобразования");
-
-    // Вывод суммы чисел
+    io::stdin().read_line(&mut num_str_1).expect("Помилка зчитуання");
+    io::stdin().read_line(&mut num_str_2).expect("Помилка зчитування");
+    let num_1: i32 = num_str_1.trim().parse().expect("Помилка перетворення ");
+    let num_2: i32 = num_str_2.trim().parse().expect("Ошибка перетворення");
     println!("{}", num_1 + num_2);
 }
