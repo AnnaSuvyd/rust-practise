c5c28f4405dc53cf61d60e47d54b29ced6b3c904
AnnaSuvyd <129731273+AnnaSuvyd@users.noreply.github.com>
2025-05-27 22:58:57 +0300

    task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,17 +1,41 @@
-pub fn draw_tree(levels: usize) {
-    let mut height = 1;
+use std::io;
 
-    for i in 0..levels {
-        for row in 0..(i + 2) {
-            let spaces = levels + (i + 1) - row - 1;
-            let stars = 2 * row + 1;
+pub fn plus_minus() {
+    let mut input = String::new();
+    
+    // Зчитуємо розмір масиву
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let n: usize = input.trim().parse().expect("Expected a number");
 
-            let line: String = std::iter::repeat(' ')
-                .take(spaces)
-                .chain(std::iter::repeat('*').take(stars))
-                .collect();
+    input.clear();
+    // Зчитуємо сам масив
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let arr: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i32>().expect("Expected number"))
+        .collect();
 
-            println!("{}", line);
+    if arr.len() != n {
+        panic!("Невідповідність розміру масиву");
+    }
+
+    let mut positive_count = 0;
+    let mut negative_count = 0;
+    let mut zero_count = 0;
+
+    for &num in &arr {
+        if num > 0 {
+            positive_count += 1;
+        } else if num < 0 {
+            negative_count += 1;
+        } else {
+            zero_count += 1;
         }
     }
+
+    let total = n as f64;
+    println!("{:.6}", positive_count as f64 / total);
+    println!("{:.6}", negative_count as f64 / total);
+    println!("{:.6}", zero_count as f64 / total);
 }
